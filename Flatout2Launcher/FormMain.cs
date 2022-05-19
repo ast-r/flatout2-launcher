b140c33232d8e267dfbbc0e0cf3a6bbd33c18d1a
ast-r <ast.r@outlook.com>
2022-05-19 18:38:43 +0500

    Code cleanup

--- Flatout2Launcher/FormMain.cs
+++ Flatout2Launcher/FormMain.cs
@@ -1,169 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 using System.Diagnostics;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Windows.Forms;
 
-namespace Flatout2Launcher
+namespace Flatout2Launcher;
+
+public partial class FormMain : Form
 {
-    public partial class FormMain : Form
-    {
-        private List<string> ipList;
-        public FormMain()
-        {
-            InitializeComponent();
+	private readonly List<string> ipList;
 
-            /* Get ip's and fill combo box */
-            ipList = new List<string>();
-            GetIPAddresses();
-        }
+	public FormMain()
+	{
+		InitializeComponent();
 
-        private void GetIPAddresses()
-        {
-            int numberOfIPs=0;
-            // Get a list of all network interfaces (usually one per network card, dialup, and VPN connection) 
-            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+		/* Get ip's and fill combo box */
+		ipList = new List<string>();
+		GetIPAddresses();
+	}
 
-            foreach (NetworkInterface network in networkInterfaces)
-            {
-                // Read the IP configuration for each network 
-                IPInterfaceProperties properties = network.GetIPProperties();
+	private void GetIPAddresses()
+	{
+		var numberOfIPs = 0;
+		// Get a list of all network interfaces (usually one per network card, dialup, and VPN connection) 
+		var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
 
-                // Each network interface may have multiple IP addresses 
-                foreach (IPAddressInformation address in properties.UnicastAddresses)
-                {
-                    // We're only interested in IPv4 addresses for now 
-                    if (address.Address.AddressFamily != AddressFamily.InterNetwork)
-                        continue;
+		foreach (var network in networkInterfaces)
+		{
+			// Read the IP configuration for each network 
+			var properties = network.GetIPProperties();
 
-                    // Ignore loopback addresses (e.g., 127.0.0.1) 
-                    if (IPAddress.IsLoopback(address.Address))
-                        continue;
+			// Each network interface may have multiple IP addresses 
+			foreach (UnicastIPAddressInformation address in properties.UnicastAddresses)
+			{
+				// We're only interested in IPv4 addresses for now 
+				if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+					continue;
 
-                    comboBox1.Items.Add(address.Address.ToString() + " (" + network.Name + ")");
-                    ipList.Add(address.Address.ToString());
-                    numberOfIPs++;
-                }
-            }
+				// Ignore loopback addresses (e.g., 127.0.0.1) 
+				if (IPAddress.IsLoopback(address.Address))
+					continue;
 
-            if (numberOfIPs > 0) {
-                comboBox1.SelectedIndex = 0;
-            }
-        }
+				comboBox1.Items.Add(address.Address + " (" + network.Name + ")");
+				ipList.Add(address.Address.ToString());
+				numberOfIPs++;
+			}
+		}
 
-        private int LaunchGame(string args) {
-            int exitCode = -1;
+		if (numberOfIPs > 0) comboBox1.SelectedIndex = 0;
+	}
 
-            // Prepare the process to run
-            ProcessStartInfo start = new ProcessStartInfo();
+	private int LaunchGame(string args)
+	{
+		var exitCode = -1;
 
-            start.Arguments = args;
-            start.FileName = "flatout2.exe";
+		// Prepare the process to run
+		var start = new ProcessStartInfo();
 
-            // Run the external process & wait for it to finish
-            try
-            {
-                using (Process proc = Process.Start(start))
-                {
-                    proc.WaitForExit();
+		start.Arguments = args;
+		start.FileName = "flatout2.exe";
 
-                    // Retrieve the app's exit code
-                    exitCode = proc.ExitCode;
-                }
-            }
-            catch (Exception e) {
-                MessageBox.Show(e.Message);
-            }
+		// Run the external process & wait for it to finish
+		try
+		{
+			using (var proc = Process.Start(start))
+			{
+				proc.WaitForExit();
 
-            return exitCode;
-        }
+				// Retrieve the app's exit code
+				exitCode = proc.ExitCode;
+			}
+		}
+		catch (Exception e)
+		{
+			MessageBox.Show(e.Message);
+		}
 
-        private bool ValidateIP(string ip) {
-            int count = 0;
-            string[] words = ip.Split('.');
+		return exitCode;
+	}
 
-            foreach (string word in words)
-            {
-                count++;
+	private bool ValidateIP(string ip)
+	{
+		var count = 0;
+		var words = ip.Split('.');
 
-                try
-                {
-                    int temp = Convert.ToInt32(word);
+		foreach (var word in words)
+		{
+			count++;
 
-                    if (temp < 0 || temp > 255)
-                    {
-                        return false;
-                    }
-                }
-                    catch (Exception e) {
-                        return false;
-                    }
+			try
+			{
+				var temp = Convert.ToInt32(word);
 
-            }
+				if (temp < 0 || temp > 255) return false;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
-            if (count != 4) {
-                return false;
-            }
+		if (count != 4) return false;
 
-            return true;
-        }
+		return true;
+	}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
+	private void button1_Click(object sender, EventArgs e)
+	{
+		Application.Exit();
+	}
 
-        private void buttonRun_Click(object sender, EventArgs e)
-        {
-             switch (mainTabControl.SelectedIndex) { 
-                /* Client */
-                case 0:
-                    if (ValidateIP(textBoxClient_ServerIP.Text))
-                    {
-                        LaunchGame("-join=" + textBoxClient_ServerIP.Text + " -lan");
-                    }
-                    else {
-                        MessageBox.Show("Invalid IP!");
-                    }
-                    break;
+	private void buttonRun_Click(object sender, EventArgs e)
+	{
+		switch (mainTabControl.SelectedIndex)
+		{
+			/* Client */
+			case 0:
+				if (ValidateIP(textBoxClient_ServerIP.Text))
+					LaunchGame("-join=" + textBoxClient_ServerIP.Text + " -lan");
+				else
+					MessageBox.Show("Invalid IP!");
+				break;
 
-                /* Server */
-                case 1:
-                    string ip = ipList[comboBox1.SelectedIndex];
-                    if (ValidateIP(ip))
-                    {
-                        LaunchGame("-host -lan -private_addr=" + ip);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Invalid IP!");
-                    }    
-                    break;
+			/* Server */
+			case 1:
+				var ip = ipList[comboBox1.SelectedIndex];
+				if (ValidateIP(ip))
+					LaunchGame("-host -lan -private_addr=" + ip);
+				else
+					MessageBox.Show("Invalid IP!");
+				break;
 
-                /* Options */
-                case 2:
-                    LaunchGame("-setup");
-                    break;
+			/* Options */
+			case 2:
+				LaunchGame("-setup");
+				break;
 
-                /* Single Player */
-                case 3:
-                    LaunchGame("");
-                    break;
-
-                default:
-                    break;
-            }
-            
-        }
-
-    }
+			/* Single Player */
+			case 3:
+				LaunchGame("");
+				break;
+		}
+	}
 }
